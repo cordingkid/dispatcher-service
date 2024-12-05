9437c4c2a55c235a1910cd50faf734a36fbf4c2d
codingkid <jjg1014@naver.com>
2024-12-05 21:55:13 +0900

    test: 함수 합성에 대한 통합 테스트

---
+++ src/test/java/com/polarbookshop/dispatcherservice/DispatchingFunctionsIntegrationTests.java
@@ -0,0 +1,49 @@
+package com.polarbookshop.dispatcherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.function.context.FunctionCatalog;
+import org.springframework.cloud.function.context.test.FunctionalSpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+import java.util.function.Function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@FunctionalSpringBootTest
+public class DispatchingFunctionsIntegrationTests {
+
+    @Autowired
+    private FunctionCatalog catalog;
+
+    @Test
+    void packAndLabelOrder() {
+        // FunctionCatalog에서 합성 함수를 가져온다.
+        Function<OrderAcceptedMessage, Flux<OrderDispatchedMessage>> packAndLabel =
+                catalog.lookup(Function.class, "pack|label");
+        long orderId = 121;
+
+        StepVerifier.create(packAndLabel.apply(new OrderAcceptedMessage(orderId)))      // 함수에 대한 입력인 OrderAcceptedMessage 정의
+                .expectNextMatches(dispatchedOrder ->                                   // 함수의 출력이 OrderDispatchedMessage 객체인지 확인한다.
+                        dispatchedOrder.equals(new OrderDispatchedMessage(orderId)))
+                .verifyComplete();
+    }
+
+    @Test
+    void packOrder() {
+        Function<OrderAcceptedMessage, Long> pack = catalog.lookup(Function.class, "pack");
+        long orderId = 121;
+        assertThat(pack.apply(new OrderAcceptedMessage(orderId))).isEqualTo(orderId);
+    }
+
+    @Test
+    void labelOrder() {
+        Function<Flux<Long>, Flux<OrderDispatchedMessage>> label = catalog.lookup(Function.class, "label");
+        Flux<Long> orderId = Flux.just(121L);
+
+        StepVerifier.create(label.apply(orderId))
+                .expectNextMatches(dispatchedOrder ->
+                        dispatchedOrder.equals(new OrderDispatchedMessage(121L)))
+                .verifyComplete();
+    }
+}
